c7c3e57990e230f7b890e1466ee94f628d68fe9f
NicksComingUp <remember.marsman@gmail.com>
2019-01-30 06:36:05 -0600

    Added create ,retrieve, update, partial_update and delete

--- src/profile_project/profile_api/urls.py
+++ src/profile_project/profile_api/urls.py
@@ -4,12 +4,13 @@ from rest_framework.routers import DefaultRouter
 from . import views
 router = DefaultRouter()
 router.register(
-'hello-viewset',
- views.HelloViewSet,
+'hello-viewset', #name of or api
+ views.HelloViewSet, # name of viewset
  base_name='hello-viewset'
  )
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    #router create the url for us automatically generate
     url(r'', include(router.urls))
 ]
