d6474853e59c6528225dfec66cfc3985f2a60598
NicksComingUp <remember.marsman@gmail.com>
2019-01-30 20:05:03 -0600

    Added permissions and authentication to our UserProfileViewSet.

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -4,8 +4,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+# most effective way to authenticate with the api
+from rest_framework.authentication import  TokenAuthentication
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 # . import from the root directory
@@ -27,7 +30,7 @@ class HelloApiView(APIView):
         if serializer.is_valid():
             name = serializer.data.get('name')
             message = 'Hello {0}'.format(name)
-            return Response({'message': message})
+            return Response({message: message})
         else:
             return Response(
             serializer.errors,
@@ -36,13 +39,13 @@ class HelloApiView(APIView):
     #pk - primary key
     def put(self, request, pk=None):
         """Handles updating an object."""
-        return Response({'method':'put'})
+        return Response({method:'put'})
     def patch(self,request, pk=None):
         """Patch requet, only updates fields provided in the request."""
-        return Response({'method': 'patch'})
+        return Response({method: 'patch'})
     def delete(self,request,pk=None):
         "Deletes an object."
-        return Response({'method':'delete'})
+        return Response({method:'delete'})
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API VewSet."""
@@ -71,16 +74,16 @@ class HelloViewSet(viewsets.ViewSet):
              )
     def retrieve(self, request, pk=None):
         """Handles getting an object by its ID."""
-        return Response({'http_method':'GET'})
+        return Response({http_method:'GET'})
     def update(self, request, pk=None):
         """Handles updating an object"""
-        return Response({'http_method':'PUT'})
+        return Response({http_method:'PUT'})
     def partial_update(self, request, pk=None):
         """Handles updating part of an object."""
-        return Response({'http_method':'PATCH'})
+        return Response({http_method:'PATCH'})
     def destroy(self, request, pk=None):
         """Handles removing an object"""
-        return Response({'http_method':'DELETE'})
+        return Response({http_method:'DELETE'})
 
 
 
@@ -88,3 +91,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, reading and updating profiles."""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    #Adding authenticate and permission to the view set
+    authentication_classes = (TokenAuthentication,) # toople
+    permission_classes = (permissions.UpdateOwnProfile,)
